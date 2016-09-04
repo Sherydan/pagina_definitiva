0f63bcbda75181e1586186ede03191d28654617e
Luis Tobar <tobar.luis.c@gmail.com>
2016-09-03 22:16:53 -0300

    cambie de nombre footer.html a footer.php (estaba trabajando con el footer erroneo

--- footer.php
+++ footer.php
@@ -1,54 +1,74 @@
 <!-- Footer ------------------------------------------------------ -->
-<!--<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
-<link href="bootstrap/css/footer-distributed-with-address-and-phones.css" rel="stylesheet" media="screen">
+<link href="assets/css/bootstrap.css" rel="stylesheet"/>
+<link href="assets/css/bootstrap-responsive.css" rel="stylesheet"/>
 <hr class="soft">
-<div  id="footerSection">
- <div class="footer-distributed">
- 	<div class="row">
-		<div class="span3">
-			<h5>Informacion</h6>
-			<a href="?open=CaraMember"><font  color="Blue">Ser Miembro?</font></a>
-			<a href="?open=KeuntunganMember"><font color= "blue">Ventajas Miembro</font></a> 
-			<a href="?open=CaraBelanja"><font color="blue">Hacer Compras?</font></a> 
-			<a href="?open=CaraPengembalian"><font color="blue">Devoluciones</font></a>  
-			<a href="#"><font color="blue">Carrera</font></a>
-		 </div>
-		<div class="span3">
-			<h5>Contacto</h5>
-            <a href="?open=KontakKami"><font color="blue">Contáctenos</font></a>    
-			<a href="?open=TermQuestion"><font color="blue">Terminos y Condiciones</font></a> 
-			<a href="#"><font color="blue">F.A.Q.</font></a>
-		 </div>
-		<div class="span3">
-			<h5>Explorar</h5>
-			<a href="?open=SyaratKetentuan"><font color="blue">Términos y Condiciones</font></a> 
-			<a href="#"><font color="blue">Politica de Privacidad</font></a>  
-			
-		 </div>
-		<div id="socialMedia" class="span3 pull-right">
-			<h5>Redes Sociales </h5>
-			<a href="#"><img width="60" src="assets/img/facebook.png" title="facebook"/></a>
-			<a href="#"><img width="60" src="assets/img/twitter.png" title="twitter"/></a>
-			<a href="#"><img width="60" src="assets/img/rss.png" title="rss"/></a>
-			<a href="#"><img width="60" src="assets/img/youtube.png" title="youtube"/></a>
-		 </div> 
-	 </div>
-	 <hr class="soft">
-	<p class="pull-left">&copy;2016 <font color= "red"><b>Platea21</b></font> <font color="green"><b>Platea21</b></font> <font color="grey"><b>Tienda Online</b></font></p>
-
- 	
- </div>
-	
-</div><!-- /container -->
+<div class="footer">
+      <div class="wrap">  
+       <div class="section group">
+        <div class="col_1_of_4 span_1_of_4">
+            <h4>Informacion</h4>
+            <ul>
+            <li><a href="about.html">Acerca</a></li>
+            <li><a href="contact.html">Servicio al Cliente</a></li>
+            <li><a href="#">Busqueda Ananzada</a></li>
+            <!--<li><a href="delivery.html">Orders and Returns</a></li>-->
+            <li><a href="contact.html">Contacto</a></li>
+            </ul>
+          </div>
+        <div class="col_1_of_4 span_1_of_4">
+          <h4>Como Comprar con nosotros</h4>
+            <ul>
+            <li><a href="about.html">Acerca</a></li>
+            <li><a href="contact.html">Servicios</a></li>
+            <li><a href="#">Politica de Privacidad</a></li>
+          <!--<li><a href="delivery.html">Orders and Returns</a></li>-->
+            <li><a href="#">Buscar Terminos</a></li>
+            </ul>
+        </div>
+        <div class="col_1_of_4 span_1_of_4">
+          <h4>Mi Cuenta</h4>
+            <ul>
+              <li><a href="contact.html">Registrarse</a></li>
+              <li><a href="index.html">Ver Tarjeta</a></li>
+                  <li><a href="#">Ver mis ordenes</a></li>
+              <li><a href="contact.html">Ayuda</a></li>
+            </ul>
+        </div>
+        <div class="col_1_of_4 span_1_of_4">
+          <h4>Contact0</h4>
+            <ul>
+              <li><span>+569-99675432</span></li>
+              <li><span>+569-99675435</span></li>
+            </ul>
+            <div class="social-icons">
+              <h4>Siguenos en:</h4>
+                  <ul>
+                    <li><a href="#" target="_blank"><img src="images/facebook.png" alt=""></a></li>
+                    <li><a href="#" target="_blank"><img src="images/twitter.png" alt=""></a></li>
+                    <li><a href="#" target="_blank"><img src="images/skype.png" alt=""> </a></li>
+                    <li><a href="#" target="_blank"> <img src="images/dribbble.png" alt=""></a></li>
+                    <li><a href="#" target="_blank"> <img src="images/linkedin.png" alt=""></a></li>
+                    <div class="clear"></div>
+                 </ul>
+              </div>
+        </div>
+      </div>      
+        </div>
+        <div class="copy_right">
+        <p>Compañia San fernando Store  © All rights Reseverd | Design by  <a href="#">Ingenieria Informatica AIEP 2016</a> </p>
+       </div>
+    </div>
 
 
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
     <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
     <script src="assets/js/jquery.js"></script>
-	<script src="assets/js/google-code-prettify/prettify.js"></script>
+  <script src="assets/js/google-code-prettify/prettify.js"></script>
     <script src="assets/js/application.js"></script>
     <script src="assets/js/bootstrap-transition.js"></script>
     <script src="assets/js/bootstrap-modal.js"></script>
@@ -64,8 +84,8 @@
     <script src="assets/js/bootstrap-typeahead.js"></script>
     <script src="assets/js/bootstrap-affix.js"></script>
     <script src="assets/js/jquery.lightbox-0.5.js"></script>
-	<script src="assets/js/bootsshoptgl.js"></script>
-	 <script type="text/javascript">
+  <script src="assets/js/bootsshoptgl.js"></script>
+   <script type="text/javascript">
     $(function() {
         $('#gallery a').lightBox();
     });
