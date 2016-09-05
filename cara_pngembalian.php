5677a5bb4ace9233fc569b189061217b3200c238
radge91 <edgardo.ig.avila@gmail.com>
2016-09-05 01:02:58 -0300

    faq +envios
    gg

--- cara_pngembalian.php
+++ cara_pngembalian.php
@@ -11,6 +11,16 @@
           p{
             text-indent:50px;
           }
+
+         div h3 {
+            color:red;
+          }
+        div  h4 {
+          color:green;
+          }
+          div p {
+          text-align: justify;
+          }
       </style>
     <!-- Le styles  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet"/>
@@ -42,20 +52,97 @@
 <div class="span9" id="mainCol">
 		<ul class="breadcrumb">
 			<li><a href="?open=''">Inicio</a><span class="divider">/</span></li>
-			<li class="active">Devoluciones</li>
+			<li class="active">Envios</li>
 			<li>Corporativos</li>
 		</ul>
-		<h3>Como hacer Devoluciones</h3>
+		<h3>ENVIOS Y FAQ</h3>
 		<hr class="soft">
-			<h3><font color="red">Platea21</font> <font color="green">SIT</font></h3><br/>
+			<h3><font color="green">San Fernando Store</font> <font color="green">S.A</font></h3><br/>
 	
         <font face:'comic sans';>
-<strong>Devoluciones</strong><br>
-<p>
+<h3>Envios</h3><br>
+<
 Si hay elementos que no encajan con lo que quieres, entonces usted puede realizar el procedimiento retorna de conformidad con los requisitos de nosotros.
  Puedes contactar directamente con el administrador de comercio electrónico con nosotros.
-</p>		
+
+   <div class="content">
+        <div class="section group">
+                <div class="grid_1_of_3 images_1_of_3">
+                      <img src="images/delivery-img1.jpg" alt="" />
+                      <h3 >Cotizar envíos</h3>
+                      Usando nuestro Cotizador, podrás simular tu envío y conocer el valor aproximado.
+Debes ingresar las características de tu envío y te entregaremos información del envio de tus compras a tu hogar.
+                </div>
+                <div class="grid_1_of_3 images_1_of_3">
+                      <img src="images/delivery-img2.jpg" alt="" />
+                      <h3>Estado de tus envíos </h3>
+                      San Fernando Store cuenta con distintos servicios que permiten la entrega de envíos a lo largo de todo la region.
+                </div>
+                <div class="grid_1_of_3 images_1_of_3">
+                      <img src="images/delivery-img3.jpg" alt="" />
+                      <h3>Embalajes </h3>
+                      Somos una compañía preocupada por el Medioambiente, no sólo en nuestra gestión cotidiana, sino también en los distintos productos que tenemos disponibles para los que, día a día, usan nuestros servicios.
+
+                </div>
+            </div>  
+        <div class="faqs">
+          <h3>Perguntas Frecuentes</h3>             
+            <div class="panel margin-lr-7">
+             
+                     <h4 class="post-title" > <h4>1.¿Se puede modificar  la direccion de envio?</h4></h4>
+                </div>
+                <div class="panel margin-lr-7">
+                    Sólo los clientes con Tarjeta de Cliente San Fernando Store pueden modificar la dirección de envío. Este cambio es posible sólo si el envío se encuentra en una etapa del proceso en la que se pueda modificar el trayecto inicial. Todos los cambios están sujetos a confirmación.
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 2.¿Si el destinatario no se encuentra puede recibirlo otra persona?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                    Si, lo puede recibir cualquier persona que esté en el domicilio y que sea mayor de edad. Deberá firmar la recepción conforme del envío.</p
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 3.¿Cuales son los metodos de pago online?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                     Cuando utilizas un servicio online en www.sanfernandostore.cl, puedes pagar con tarjetas de crédito o débito, y con transferencia electrónica.
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 4.¿Si soy menor de edad puedo retirar un producto?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                      No,  sólo pueden enviarse a personas mayores de 18 años, quienes deben presentar su documento de identificación al momento de retirarlo
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 5.¿Si perdi mi contraseña puedo recuperarla?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                     Puedes recuperar tu contraseña en nuestro sitio web www.sanfernandostore.cl. Te enviaremos un correo electrónico con la información a la dirección con la que te registraste.
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 6.¿Horarios de atencion?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                     Las mayoría de las sucursales de San Fernando Store atienden de 9:00 a 22:00 horas, de lunes a viernes, y los días sábado de 9:00 a 21:00 horas. 
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 7.¿Cuanto debo pagar por envio a domicilio?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                      Nada. Los envíos se pagan con las compras con la tarjeta, ya que nuestros empleados no pueden recibir dinero.
+                </div>
+                <div class="tab bar">
+                    <h4 class="post-title"> 8.¿Como puedo obtener una tarjeta San Fernando Store?</h4>
+                </div>
+                <div class="panel margin-lr-7">
+                     Para tener tu Tarjeta de Crédito San Fernando Store, debes registrarte en nuestro sitio www.SanfernandoStore.cl 
+                </div>
+               </div>
+              </div>    
+         </div> 
+    </div>
+ </div>	
 </div>
 </div>
+
 </body>
 </html>
