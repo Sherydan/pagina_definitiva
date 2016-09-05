306e05dda77a74ccc01443d4f22350600a09afd2
radge91 <edgardo.ig.avila@gmail.com>
2016-09-05 00:33:31 -0300

    CAMBIO VNTAJAS DE SER MIEMBRO

--- keuntungan_member.php
+++ keuntungan_member.php
@@ -41,14 +41,37 @@
 			<li class="active">Como ser Miembro</li>
 			<li>Corporativo</li>
 		</ul>
-		<h3>Ventajas de ser miembro</h3>
+<h3><font color="green">Ventajas de ser miembro  Ventajas online</font></h3><br>
 		<hr class="soft">
-			<h3><font color="red">Platea21</font> <font color="green">SIT</font></h3><br/>
+			<h3><font color="red">San Fernando Store</font> <font color="green">S.A</font></h3><br/>
 	<p>
-        <font face:'comic sans';>
-<strong>Ventajas de ser miembro</strong><br><br>
-Hay tantas ventajas que usted puede ser capaz de convertirse en un miembro de Platea21 Online. Usted puede hacer compras de una manera que es fácil para sus necesidades. También puede determinar fácilmente el presupuesto que tiene que pasar rápidamente. También puede consultar directamente y hacer preguntas directamente a nuestro personal a través de sistemas de chat que existen en nuestro.
-<br><br>
+
+
+
+
+
+
+<h4><font color="red"> 1.- Reduce costos </font></h4>.Abrir una tienda virtual es lógicamente más económico, pues permite a las empresas crecer con menos recursos al evitar el pago de renta del local, servicios, sueldos, etcétera. También es más económico  porque al anunciar sus productos online, están haciendo publicidad por Internet,  el cual es un medio de promoción más barato que la publicidad la cual se utiliza en medios de comunicación tradicionales.
+<br>
+<br>
+<h4><font color="red">2.- Genera lealtad con los clientes</font></h4> No existen muchas empresas que ofrezcan una buena  experiencia de compra  electrónica, incluso todavía hay miles que no tienen presencia en línea.  Esta es una oportunidad para lograr que tus clientes disfruten esa experiencia, a través de ofrecerles un amplio catálogo de productos,  buenos precios y facilidad en el uso de las plataformas, así como las  diversas opciones de pago principalmente.
+<br>
+<br>
+<h4><font color="red">3.- Atención y Garantía de Satisfacción Total</font></h4> Los sitios de e-commerce conocen la importancia de la atención al cliente y  saben que en la mayoría de los casos hasta no ver o tocar un producto es como se toma la decisión final de compra, por ello cuenta con un chat para asesorarte durante la elección del producto, así como una Garantía de Satisfacción Total, en la que los clientes tienen 30 días después de que les llegue el producto para devolverlo y pedir el reembolso de su dinero si los artículos no cumplieron con sus expectativas. Si un cliente está satisfecho con su experiencia de compra del algún producto seguramente regresará al sitio y hará su recomendación a través del “word of mouth”.
+<br>
+<br>
+<h4><font color="red">4.- Mayor alcance de público</font></h4> Al comprar y vender  por Internet, los productos están al alcance de todos desde cualquier lugar y a cualquier hora los 365 días de año. Además, en Internet hay más de 350 millones de usuarios activos, es decir cuenta con un público mucho mayor que los que podrían transitar por una tienda física. Sin lugar a dudas, las posibilidades de crecimiento y el alcance de internet son espectaculares.
+<br>
+<br>
+<h4><font color="red">5.- Seguridad para el vendedor y el comprador</font></h4> Una de las principales causas por las que los internautas aún no se atreven a comprar en línea, es por el desconocimiento y/o desconfianza  en el uso de los sistemas de pago online; sin embargo, vendiendo a través de plataformas electrónicas tienes la seguridad de que cuenta con el sello de confianza otorgado por Amipci.  Con esto proteges los datos de tus usuarios y  evitas que los de su tarjeta sean utilizados con fines maliciosos.  Si los usuarios tienen la seguridad de realizar compras online con su tarjeta de crédito, las ventas por ende aumentan, pues los clientes podrán disfrutar de grandes beneficios como rebajas y/o facilidades de pago.
+<br>
+<br>
+<h4><font color="red">6.- Facilidad de entrega de productos</font></h4> Algunos usuarios consideran que un riesgo de las compras por Internet, es que la mercancía no llegue bien o a tiempo, pero cada vez existen más empresas de logística que ofrecen servicios de entrega rápidos, eficaces y seguros, con precios accesibles.  También puedes utilizar una plataforma que cuente con rastreo de pedido, para que tus clientes comprueben el estado del envío de su producto, y tengan la  confianza de que su compra llegara en tiempo y forma.
+<br>
+<br>
+<h4><font color="red">7.-Mayor participación en la cartera de los clientes</font></h4> Otro de los beneficios que reciben las empresas al vender por Internet, es que generan mayor participación en la cartera de los clientes, pues el 50% de los usuarios investiga los productos en Internet antes de buscarlos en la tienda física, por lo que es importante para cualquier compañía crear vínculos e interacción con el consumidor a través de internet, pues es una parte relevante en  su proceso de compra. - Read more at: http://scl.io/24AZzhSF#gs.KCfk3fo
+<br>
+<br>
 </p>		
 </div>
 </div>
