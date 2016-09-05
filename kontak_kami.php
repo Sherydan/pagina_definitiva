bbc7026f77ec13aad402380923ec181bdfa460fb
radge91 <edgardo.ig.avila@gmail.com>
2016-09-05 00:08:36 -0300

    contactanos traducido  y con mapa
    gg

--- kontak_kami.php
+++ kontak_kami.php
@@ -47,18 +47,22 @@
 		</ul>
 		<h3>Contactenos</h3>
 		<hr class="soft">
-			<h3><font color="red">Platea21</font> <font color="green">SIT</font></h3><br/>
+			<h3><font color="red">San Fernando Store</font> <font color="green">S.A</font></h3><br/>
 	
         <font face:'comic sans';>
 <strong>Contactenos</strong><br>
 <p>
-Como  Tienda Online están cerca de nuestros clientes también abriría oportunidades para dirigir usted cerca Radja Edificio en línea a través del número de teléfono móvil RPM #952505086. También puede contactar con nosotros a través del correo electrónico: julioscsar@gmail.com
-Puede preguntar acerca de nuestros productos que van desde el precio hasta que el depósito de bienes disponibles.
-Máximo servicio a los consumidores es el objetivo de nuestros servicios de comercio.</p> 
+Empresas San fernando Store nace como un portal del comercio de la cuidad San fernando. Esta página pretende llegar a todas las ciudad San fernando con una web moderna, donde con un solo clic puedas acceder de forma muy rápida a la información de cualquier información de sus productos, con ofertas, características, precios, etc. Esperamos que en poco tiempo se vayan incorporando todos los comercios de nuestra comunidad y de esta forma conseguir que, Sanfernandostore.com sea el mayor directorio del comercio en San fernandos.</p> 
  <div class="span4">
-	<iframe  width="790" height="250" frameborder="0" scrolling="no" marginheight="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1689.8452160504075!2d-70.23926489999967!3d-17.99962810251452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915ac58049c602cd%3A0x266b1025d302723e!2sCalle+Sinchi+Roca+155%2C+Tacna!5e0!3m2!1ses!2spe!4v1448990308837" width="1170" height="220" frameborder="0" style="border:0"></iframe><br /><small><a href="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3966.2345099489826!2d106.992008!3d-6.232786!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1c81b202e00b1a7a!2sRadja+Bangunan!5e0!3m2!1sid!2sid!4v1432107355716" style="color:#0000FF;text-align:left">Ver Mapa Más Grande</a></small>
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13138.070588024088!2d-70.9896066835162!3d-34.59106967252907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9664c7b3736834b3%3A0x4af1bd3814cb2a06!2sSan+Fernando%2C+VI+Regi%C3%B3n!5e0!3m2!1ses-419!2scl!4v1472263306216" width="800" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
+      <font color="red">Conmicarse</font><br>
+          <font color="green"> Telefono:072-776544 </font> <br>
+          <font color="green"> Correo. sanfernandostore@ecomerce.cl</font>
+
+
 	</div>
 </div>
+
 </div>
 </body>
 </html>
